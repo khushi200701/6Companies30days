10943de777a1dfa3f9bd39571c820e8c5f265d34
Khushi <58480229+khushi200701@users.noreply.github.com>
2022-01-10 21:50:35 +0530

    Create Serialize and deserialize BT.java

---
+++ 06-10 Amazon/Serialize and deserialize BT.java
@@ -0,0 +1,30 @@
+// https://practice.geeksforgeeks.org/problems/serialize-and-deserialize-a-binary-tree/1
+
+class Tree 
+{
+    public void serialize(Node root, ArrayList<Integer> A) 
+{
+   
+    if(root!=null)
+    A.add(root.data);
+    else {
+        A.add(-1);
+        return;
+    }
+    serialize(root.left,A);
+    serialize(root.right,A);
+}
+
+int idx = 0;
+   public Node deSerialize(ArrayList<Integer> A)
+   {
+       if(idx>=A.size()||A.get(idx)==-1) {
+           idx++;
+           return null;
+       }
+       Node root = new Node(A.get(idx++));
+       root.left = deSerialize(A);
+       root.right = deSerialize(A);
+       return root;
+   }
+};
