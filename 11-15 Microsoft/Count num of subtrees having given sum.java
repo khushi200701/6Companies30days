f9b93e078843af746e10f3eed0eaaeea5c1451e8
Khushi <58480229+khushi200701@users.noreply.github.com>
2022-01-13 23:31:18 +0530

    Create Count num of subtrees having given sum.java

---
+++ 11-15 Microsoft/Count num of subtrees having given sum.java
@@ -0,0 +1,22 @@
+// https://practice.geeksforgeeks.org/problems/count-number-of-subtrees-having-given-sum/1/
+
+class Tree
+{
+    
+    int count;
+    private int subTreeSum(Node root, int X){
+        if(root==null) return 0;
+        int sum = root.data + subTreeSum(root.left, X) + subTreeSum(root.right, X);
+        if(sum==X) count++;
+        return sum;
+    }
+    
+    //Function to count number of subtrees having sum equal to given sum.
+    int countSubtreesWithSumX(Node root, int X)
+    {
+	//Add your code here.
+	    count = 0;
+	    subTreeSum(root, X);
+	    return count;
+    }
+}
