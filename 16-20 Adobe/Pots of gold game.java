8573131073ea7cddee21e937da6828aac9c04670
Khushi <58480229+khushi200701@users.noreply.github.com>
2022-01-20 10:34:43 +0530

    Create Pots of gold game.java

---
+++ 16-20 Adobe/Pots of gold game.java
@@ -0,0 +1,22 @@
+// https://practice.geeksforgeeks.org/problems/pots-of-gold-game/1/
+
+class GfG
+{
+  public static int maxCoins(int A[],int n)
+    {
+    int dp[][]=new int[n][n];
+    for(int row[]:dp){
+      Arrays.fill(row,-1);
+      }
+    return helper(A,n,0,n-1,dp);
+    }
+      static int helper(int A[],int n,int start,int end,int dp[][]){
+        if(start==end)
+          return dp[start][end]=A[start];
+        if(start+1==end)
+          return dp[start][end]=Math.max(A[start],A[end]);
+        if(dp[start][end]!=-1)
+          return dp[start][end];
+        return dp[start][end]=Math.max(A[start]+Math.min(helper(A,n,start+2,end,dp),helper(A,n,start+1,end-1,dp)),A[end]+Math.min(helper(A,n,start+1,end-1,dp),helper(A,n,start,end-2,dp)));
+  }
+}
